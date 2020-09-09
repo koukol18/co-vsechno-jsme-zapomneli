28d5c84b211ff92bea2464f56193cb0140ad5d09
Milan Koukol <milankoukol88@gmail.com>
2020-09-09 11:23:57 +0000

    added lot of code

--- script.js
+++ script.js
@@ -18,4 +18,32 @@ const zaci = [
   {jmeno: 'Dominik Lederer',rok: 2003, trida: '3.G'},
   {jmeno: 'Majk Svoboda',rok: 2001, trida: '3.G'},
   {jmeno: 'Petr Zdražil',rok: 2002, trida: '3.G'}
-];
+];/*
+for (let index = 0; index < zaci.length; index++) {
+
+  if ((2020 - zaci[index].rok) >= 18) {
+  console.log(zaci[index].jmeno + " je plnoletý.")
+} else {
+  console.log(zaci[index].jmeno + " není plnoletý.")
+};
+};
+
+console.log("-------------");
+*/
+
+zaci.forEach (zak => {
+  let stav = ((2020 - zak.rok) >= 18) ? " je plnoletý." : " není plnoletý.";
+  console.log(zak.jmeno + stav);
+
+  let p = document.createElement("p");
+  p.textContent = zak.jmeno + stav;
+  if ((2020 - zak.rok) >= 18) {
+  console.log(zak.jmeno + " je plnoletý.")
+  p.style.color = "green";
+} else {
+  console.log(zak.jmeno + " není plnoletý.")
+  p.style.color = "red";
+};
+  document.body.appendChild(p);
+})
+
