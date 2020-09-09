4fda6b64d3050148c25f637f76458a4a4c4a6152
Milan Koukol <milankoukol88@gmail.com>
2020-09-09 10:41:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+console.log("Hello world!!");
+
+console.log("-------------");
+
+const rokNarozeni = 2002;
+
+if ((2020 - rokNarozeni) >= 18) {
+  console.log("Je plnoletý")
+} else {
+  console.log("Není plnoletý")
+}
+
+console.log("-------------");
+
+const zaci = [
+  {jmeno: 'Dominik Lederer',rok: 2003, trida: '3.G'},
+  {jmeno: 'Majk Svoboda',rok: 2001, trida: '3.G'},
+  {jmeno: 'Petr Zdražil',rok: 2002, trida: '3.G'}
+];
